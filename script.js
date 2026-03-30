af5ecf2ae12bcfab1b02b9740e2b9b7aa77894eb
Rémi Maloberti <remimaloberti@macbookair-1.home>
2026-03-30 11:36:18 +0200

    update script

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+// Scroll bouton
 function scrollToContact() {
     document.getElementById("contact").scrollIntoView({
         behavior: "smooth"
     });
 }
+
+// Animation au scroll
+const elements = document.querySelectorAll(".fade-in");
+
+function showOnScroll() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    elements.forEach(el => {
+        const boxTop = el.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            el.classList.add("visible");
+        }
+    });
+}
+
+window.addEventListener("scroll", showOnScroll);
+
+// lancer au chargement
+showOnScroll();
