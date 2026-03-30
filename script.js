2b567c93c85edf8555021512d3746a1446c1bf45
Rémi Maloberti <remimaloberti@macbookair-1.home>
2026-03-30 11:45:54 +0200

    update hero text

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Scroll button
 function scrollToContact() {
-    document.getElementById("contact").scrollIntoView({
-        behavior: "smooth"
-    });
+  document.getElementById("contact").scrollIntoView({
+    behavior: "smooth",
+  });
 }
 
 // Animation on scroll
 const elements = document.querySelectorAll(".fade-in");
 
 function showOnScroll() {
-    const triggerBottom = window.innerHeight * 0.85;
+  const triggerBottom = window.innerHeight * 0.85;
 
-    elements.forEach(el => {
-        const boxTop = el.getBoundingClientRect().top;
+  elements.forEach((el) => {
+    const boxTop = el.getBoundingClientRect().top;
 
-        if (boxTop < triggerBottom) {
-            el.classList.add("visible");
-        }
-    });
+    if (boxTop < triggerBottom) {
+      el.classList.add("visible");
+    }
+  });
 }
 
 window.addEventListener("scroll", showOnScroll);
